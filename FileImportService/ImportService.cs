2fe2a757a10b1fdbe89ba60790edc23129ad110e
Cordney <Cordney@DESKTOP-BDT726O>
2016-12-04 20:11:16 -0500

    Add project files.

---
+++ FileImportService/ImportService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileImportService
+{
+    class ImportService
+    {
+        private FileSystemWatcher _watcher;
+
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher(@"C:\Users\Cordney\Documents\FileTest");
+
+            _watcher.Created += FileCreated;
+
+            _watcher.IncludeSubdirectories = false;
+
+            _watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            string FileName = e.Name;
+            Console.WriteLine(FileName);
+            File.Delete(e.FullPath);    
+        }
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+
+            return true;
+        }
+    }
+}
---
+++ FileImportService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace FileImportService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<ImportService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(
+                        () => new ImportService());
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+                });
+                serviceConfig.SetServiceName("CLE_Import_Service");
+                serviceConfig.SetDisplayName("CLE Custom Service");
+                serviceConfig.SetDescription("Custom file watcher service for import calling SSIS");
+                serviceConfig.StartAutomatically();
+            });
+        }
+    }
+}
